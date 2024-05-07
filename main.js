c198cd58f167a7c79f32ae5ff3f6b119d891fbbd
LucasFernandesF <lucasffb120@gmail.com>
2024-05-07 08:31:28 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+    $('#carousel-imagens').slick({
+        autoplay: true,
+        arrows: false
+    });
+
+    $('.menu-hamburguer').click(function () {
+        $('nav').slideToggle();
+    })
+
+    $('#telefone').mask('(00)00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true,
+                minlength: 14
+            },
+            veiculoInteresse: {
+                required: false
+            },
+            mensagem: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function (form) {
+            console.log(form)
+        },
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            console.log(camposIncorretos)
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+
+    $('.lista-veiculos button').click(function () {
+        const destino = $('#contato');
+
+        const nomeVeiculo = $(this).parent().find('h3').text()
+
+        $('#veiculo-interesse').val(nomeVeiculo)
+
+        $('html').animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+
+})
